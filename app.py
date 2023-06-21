448aa17d0953de70c3d25bff0ab875eae7c8af58
Bereket-1 <74247805+Bereket-1@users.noreply.github.com>
2023-06-21 14:52:28 +0000

    Made some changes

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ JOBS = [
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Jovian')
+  return render_template('home.html', jobs=JOBS, company_name="Bereket's")
 
 @app.route("/jobs")
 def list_jobs():
